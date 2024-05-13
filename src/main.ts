964f5ad58614e724b3b9bc59213b2d30d4f91972
Svein Are Danielsen <svein.danielsen@inmeta.no>
2024-05-13 16:47:28 +0200

    refactor: case

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+  await app.listen(port);
 }
 bootstrap();
